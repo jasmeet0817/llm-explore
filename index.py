e69886b21e1806aaa902876b1c8ea456a2c7728d
Jasmeet Singh <jasmeet0817@gmail.com>
2024-01-13 17:46:36 +0100

    Some fixes

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__, template_folder='client')
 app.register_blueprint(lca_chat)
 
 # Set the port number and certificate/key file paths
-port = 5000
+port = 80
 # cert_file = 'ssl-certs/fullchain.pem'
 # key_file = 'ssl-certs/privkey.pem'
 
